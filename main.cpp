e69d2ed45bb86571ff8b64445fbb6a23e2320a05
Versatile75 <Versatile75@Versatile75s-MacBook-Pro.local>
2016-07-08 19:22:07 +0900

    07/08 진행 내용.

--- main.cpp
+++ main.cpp
@@ -1,104 +1,46 @@
-/*
- 기능 설계 5/24
- 
- main
- 
- 1. 사용자 등록(최대인원 5명제한) -> 카메라 작동 -> xml기반으로 얼굴영역찾기 -> 얼굴영역 캡쳐로 얼굴사진파일생성 (살짝왼쪽, 살짝오른쪽 옆얼굴, 정면 대략 10장정도.) -> 사진크기조정(100*149) -> 사진데이터베이스를 사용해서 평균얼굴파일생성. -> 사용자비밀번호등록
- 
- 2. 로그인 -> 얼굴로로그인할것인지, passward로 로그인할것인지 -> 카메라 작동 -> xml기번으로 얼굴영역찾기
- -> 얼굴영역 캡쳐로 얼굴사진파일생성(한5초동안 사진5장정도 생성) -> 사진크기조정 -> DB의등록된 평균얼굴들과 찍은사진 비교로 판단.
- 
- */
-
-#define _CRT_SECURE_NO_WARNINGS
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Versatile75 on 2016. 2. 4..
+//  Copyright © 2016년 Versatile75. All rights reserved.
+//
 
 #include <iostream>
-#include <cstdio>
+#include <stdio.h>
 
+#include <opencv2/opencv.hpp>
 #include <opencv/cv.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
 #include <opencv/cvaux.h>
+
 #define LINE_HEIGHT		12
 #define MARGIN_LEFT		10
 #define MARGIN_BOTTOM	10
 #define VIEW_WIDTH	640
 #define VIEW_HEIGHT	480
 
-
 using namespace std;
 using namespace cv;
 
 /** Function Headers */
 void detectAndDisplay( Mat frame );
-void ShowMenu(IplImage* img);
+void ShowMenu(Mat img);
 void keySelectMenu(char c);
-bool bVisibleMenu = 1;
 
 /** Global variables */
 CvFont* font = new CvFont;
-const float font_size = 0.7f;
-const int width = 640, height = 480;
+const float font_size = 0.3f;
+const int width = 640, height = 380;
+bool bVisibleMenu = 1;
 
-/**
- 카메라를 실행하는 함수.
- 현재 카메라가 촬영하는 화면을 출력.
- */
- int operateCamera(){
-     
-     int select=0;
-     int c = 0;
-     IplImage *frame;
-     IplImage *image = 0;
-     IplImage *ori_image = 0;
-     IplImage *view_image = 0;
-     
-     cvInitFont(font, CV_FONT_HERSHEY_PLAIN, font_size, 1.0f, 0, 1, CV_AA);
-     
-     CvCapture* capture = cvCaptureFromCAM(0);
-     cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH, width);
-     cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT, height);
-     
-     cvNamedWindow("Test",0);
-     cvResizeWindow("Test", VIEW_WIDTH, VIEW_HEIGHT);
-     
-     
-     while(capture) {
-         ori_image = cvQueryFrame( capture );
-         image = cvCloneImage(ori_image);
-         
-         if (!view_image) {
-             view_image = cvCreateImage(cvSize(VIEW_WIDTH, VIEW_HEIGHT), image->depth, image->nChannels);
-         }
-         cvResize(image, view_image, CV_INTER_LINEAR);
-         
-//         frame = cvQueryFrame(capture);
-//         cvShowImage("Test",frame);
-         ShowMenu(view_image);
-        
-         c = cvWaitKey(10);
-         if((char)c == 27 || c =='q')
-             break;
-         keySelectMenu(c);
-         view_image->origin = image->origin;
-         cvShowImage("Test", view_image);
-         cvReleaseImage(&image);
-     }
-     
-     cvReleaseCapture(&capture);
-     cvDestroyWindow("Test");
-     delete font;
-     return 0;
-}
+String face_cascade_name = "/Users/Versatile75/Documents/XcodeWorkspace/OpenCVTest/haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "/Users/Versatile75/Documents/XcodeWorkspace/OpenCVTest/haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+String window_name = "Face detection Test";
 
-/**
- 카메라를 실행하는 함수.
- 현재 카메라가 촬영하는 화면을 출력.
- */
-void detectAndDisplay( Mat frame )
-{
-
-}
 void keySelectMenu(char c){
     switch (c) {
         case 'l':
@@ -106,38 +48,105 @@ void keySelectMenu(char c){
             break;
         case 'c':
             break;
-        case 'm':
+        case 'h':
             bVisibleMenu = !bVisibleMenu;
             break;
         default:
             ;
     }
 }
-void ShowMenu(IplImage* img)
+void ShowMenu(Mat img)    //  IplImage* img
 {
+    //    cvInitFont(font, CV_FONT_HERSHEY_PLAIN, font_size, 1.0f, 0, 1, CV_AA);
     int MENU_HEIGHT = MARGIN_BOTTOM;
     
-    cvPutText(img, "Graduation", cvPoint(VIEW_WIDTH - 88, VIEW_HEIGHT - 23), font, CV_RGB(255,0,0));
-
-    cvPutText(img, "Project", cvPoint(VIEW_WIDTH - 68, VIEW_HEIGHT - 10), font, CV_RGB(255,0,0));
-
+    putText(img, "Graduation", cvPoint(VIEW_WIDTH-88, VIEW_HEIGHT-23), 2, font_size, CV_RGB(255, 0, 0));
+    putText(img, "Project", cvPoint(VIEW_WIDTH - 68, VIEW_HEIGHT - 10), 2, font_size, CV_RGB(255,0,0));
     
     if(bVisibleMenu){
-        cvPutText(img, "q - quit", cvPoint(MARGIN_LEFT, MENU_HEIGHT), font, CV_RGB(255,0,0));
+        putText(img, "q - quit", cvPoint(MARGIN_LEFT, MENU_HEIGHT), 2, font_size, CV_RGB(255,0,0));
+        MENU_HEIGHT += LINE_HEIGHT;
+        putText(img, "h - hide menu", cvPoint(MARGIN_LEFT, MENU_HEIGHT), 2, font_size, CV_RGB(255,0,0));
+        MENU_HEIGHT += LINE_HEIGHT;
+        putText(img, "l - login", cvPoint(MARGIN_LEFT, MENU_HEIGHT), 2, font_size, CV_RGB(255,0,0));
+        MENU_HEIGHT += LINE_HEIGHT;
+        putText(img, "c - create Account", cvPoint(MARGIN_LEFT, MENU_HEIGHT), 2, font_size, CV_RGB(255,0,0));
         MENU_HEIGHT += LINE_HEIGHT;
         
-        cvPutText(img, "m - hide menu", cvPoint(MARGIN_LEFT, MENU_HEIGHT), font, CV_RGB(255,0,0));
-        MENU_HEIGHT += LINE_HEIGHT;
-        
-        cvPutText(img, "l - login", cvPoint(MARGIN_LEFT, MENU_HEIGHT), font, CV_RGB(255,0,0));
-        MENU_HEIGHT += LINE_HEIGHT;
-        cvPutText(img, "c - create Account", cvPoint(MARGIN_LEFT, MENU_HEIGHT), font, CV_RGB(255,0,0));
-        MENU_HEIGHT += LINE_HEIGHT;
-
     } else {
-        cvPutText(img, "m - show menu", cvPoint(MARGIN_LEFT, MENU_HEIGHT), font, CV_RGB(255,0,0));
+        putText(img, "h - show menu", cvPoint(MARGIN_LEFT, MENU_HEIGHT), 2, font_size, CV_RGB(255,0,0));
         MENU_HEIGHT += LINE_HEIGHT;
     }
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+    
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+    
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+        
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+        
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+        
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
+}
+
+//-- 2. Read the video stream
+int operateCamera(){
+    VideoCapture capture;
+    Mat frame;
+    int c=0;
+    
+    capture.open( -1 );
+    if ( ! capture.isOpened() ){
+        printf("--(!)Error opening video capture\n"); return -1;
+    }
+    
+    while ( capture.read(frame) )
+    {
+        if( frame.empty() )
+        {
+            printf(" --(!) No captured frame -- Break!");
+            break;
+        }
+        capture.set(CV_CAP_PROP_FRAME_WIDTH, width);
+        capture.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+        resizeWindow(window_name, VIEW_WIDTH, VIEW_HEIGHT);
+        
+        //-- 3. Apply the classifier to the frame
+        ShowMenu(frame);
+        
+        detectAndDisplay( frame );
+        c= cvWaitKey(10);
+        if ((char)c == 27 || c == 'q') {
+            break;
+        }
+        keySelectMenu(c);
+        
+    }
+    return 0;
 }
 
 /**
@@ -172,10 +181,17 @@ int chooseLoginMode(){
     }
 }
 
-int main(int argc, const char * argv[]) {
+
+/** @function main */
+int main( void )
+{
+    
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
     
     chooseLoginMode();
     
     return 0;
-
 }
+
